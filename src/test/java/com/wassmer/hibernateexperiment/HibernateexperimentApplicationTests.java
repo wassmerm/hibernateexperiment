42ec5c8a9d9b3805d0b5aa092cc73f547c3dfa4e
Marcel <marcel.wassmer@gmail.com>
2023-08-19 23:14:20 +0800

    Initial commit

---
+++ src/test/java/com/wassmer/hibernateexperiment/HibernateexperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wassmer.hibernateexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateexperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
